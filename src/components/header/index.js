c889821cd0e87448991cde7c2800d938aadd6dc8
Stephen Stewart <stephen.stewart@carisenda.com>
2016-09-28 16:37:27 +0100

    drop links not in supplied design

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,7 +1,6 @@
 import React, { PropTypes, Component } from 'react';
 import { Link } from 'react-router';
 
-import NavLink from '../navlink';
 import SignIn from '../sign-in';
 
 import styles from './header.css';
@@ -17,11 +16,6 @@ export default class Header extends Component {
           <Link className={ styles.logo } to="/">
             <img src={ logo } width="107" height="24" alt="Ubuntu" />
           </Link>
-          <div className={ styles.mainNav }>
-            <NavLink to="/" onlyActiveOnIndex={ true }>Home</NavLink>
-            <NavLink to="/payment/edit">Add card</NavLink>
-            <NavLink to="/about">About</NavLink>
-          </div>
           <div className={ styles.sideNav }>
             <SignIn identity={ identity } />
           </div>
