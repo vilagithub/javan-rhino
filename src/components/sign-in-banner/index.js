4ff6fee8cce75c72587a1a49b6c036cbe785552e
Stephen Stewart <stephen.stewart@carisenda.com>
2016-09-28 11:49:14 +0100

    remove class methods

--- src/components/sign-in-banner/index.js
+++ src/components/sign-in-banner/index.js
@@ -5,32 +5,32 @@ import styles from './sign-in-banner.css';
 
 export default class SignInBanner extends Component {
 
-  _renderAuthenticated(identity) {
-    const { name, email } = identity;
-    return (
-      <div className={ styles.box }>
-        <h3>Welcome, { name }</h3>
-        <div>
-          You are signed in with the email address { email }. <a href="https://login.ubuntu.com/">Manage your SSO account</a>
-        </div>
-      </div>
-    );
-  }
-
-  _renderUnauthenticated() {
-    return (
-      <Anchor style={{ display: 'block' }} href="/login/authenticate">Sign in through Ubuntu One</Anchor>
-    );
-  }
-
   render() {
     const { identity } = this.props;
+
+    function renderAuthenticated(identity) {
+      const { name, email } = identity;
+      return (
+        <div className={ styles.box }>
+          <h3>Welcome, { name }</h3>
+          <div>
+            You are signed in with the email address { email }. <a href="https://login.ubuntu.com/">Manage your SSO account</a>
+          </div>
+        </div>
+      );
+    }
+
+    function renderUnauthenticated() {
+      return (
+        <Anchor style={{ display: 'block' }} href="/login/authenticate">Sign in through Ubuntu One</Anchor>
+      );
+    }
 
     return (
       <div className={ styles.banner }>
         { identity.isAuthenticated
-          ? this._renderAuthenticated(identity)
-          : this._renderUnauthenticated() }
+          ? renderAuthenticated(identity)
+          : renderUnauthenticated() }
       </div>
     );
   }
