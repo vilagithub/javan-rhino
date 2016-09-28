d86adab6703af695dd49ef5d1fb5e7bfe1ea84af
Stephen Stewart <stephen.stewart@carisenda.com>
2016-09-28 15:12:07 +0100

    settings for dev, staging and prod

---
+++ settings/production.js
@@ -0,0 +1,16 @@
+module.exports = {
+  UNIVERSAL: {
+    MU_URL: 'https://my.ubuntu.com',
+    STRIPE_PUBLISHABLE_KEY: 'pk_test_daBepdMharNP0PTQYoyQJPjH'
+  },
+  SERVER: {
+    UBUNTU_SSO_URL: 'https://login.ubuntu.com',
+    UBUNTU_SCA_URL: 'https://myapps.developer.ubuntu.com',
+    OPENID: {
+      VERIFY_URL: 'https://my.ubuntu.com/login/verify',
+      TEAMS: [
+        'ubuntuone-hackers'
+      ]
+    }
+  }
+};
---
+++ settings/staging.js
@@ -0,0 +1,16 @@
+module.exports = {
+  UNIVERSAL: {
+    MU_URL: 'https://my.staging.ubuntu.com',
+    STRIPE_PUBLISHABLE_KEY: 'pk_test_daBepdMharNP0PTQYoyQJPjH'
+  },
+  SERVER: {
+    UBUNTU_SSO_URL: 'https://login.staging.ubuntu.com',
+    UBUNTU_SCA_URL: 'https://myapps.developer.staging.ubuntu.com',
+    OPENID: {
+      VERIFY_URL: 'https://my.staging.ubuntu.com/login/verify',
+      TEAMS: [
+        'ubuntuone-hackers'
+      ]
+    }
+  }
+};
