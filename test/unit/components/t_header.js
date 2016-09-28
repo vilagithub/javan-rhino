068f43f4bd4e5958ef0dafb942dededd5f1658bb
Stephen Stewart <stephen.stewart@carisenda.com>
2016-09-28 16:37:49 +0100

    redirect / to /payment/edit

--- test/unit/components/t_header.js
+++ test/unit/components/t_header.js
@@ -3,7 +3,6 @@ import expect from 'expect';
 import { shallow } from 'enzyme';
 
 import Header from '../../../src/components/header';
-import NavLink from '../../../src/components/navlink';
 import SignIn from '../../../src/components/sign-in';
 
 describe('<Header /> component', () => {
@@ -11,10 +10,6 @@ describe('<Header /> component', () => {
 
   beforeEach(() => {
     header = shallow(<Header />);
-  });
-
-  it('should render links', () => {
-    expect(header.find(NavLink).length).toBeGreaterThan(0);
   });
 
   it('should render user component', () => {
