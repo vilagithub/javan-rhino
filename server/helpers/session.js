4369eb8409e36ffe69f96f3dbd2d407ae205f7da
Stephen <stephen.stewart@carisenda.com>
2016-10-05 11:47:16 +0100

    update default cookie name, per express sec best practice (#127)

--- server/helpers/session.js
+++ server/helpers/session.js
@@ -3,6 +3,7 @@ import ConnectMemcached from 'connect-memcached';
 
 const MemcachedStore = ConnectMemcached(session);
 const SESSION_DEFAULTS = {
+  name : 'sessionId',
   cookie: {
     maxAge: 2 * 60 * 60 * 1000, // 2 hours, macaroon life span
   },
