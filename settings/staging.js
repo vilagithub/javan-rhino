f48af4c0219a91f553546eae75908e12acb0b019
Wes Mason <wes@1stvamp.org>
2016-10-05 15:21:14 +0100

    Fix bind host/port for staging (#144)

--- settings/staging.js
+++ settings/staging.js
@@ -4,6 +4,8 @@ module.exports = {
     STRIPE_PUBLISHABLE_KEY: 'pk_test_daBepdMharNP0PTQYoyQJPjH'
   },
   SERVER: {
+    HOST: '0.0.0.0',
+    PORT: 8000,
     UBUNTU_SSO_URL: 'https://login.staging.ubuntu.com',
     UBUNTU_SCA_URL: 'https://myapps.developer.staging.ubuntu.com',
     OPENID: {
