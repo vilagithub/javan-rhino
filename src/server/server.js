192eb702dd4c5e68f4497c7038291f17ffa2b758
Katie Fenn <katie.fenn@googlemail.com>
2016-11-11 14:09:30 +0000

    99: Hot reloading server (#185)
    - Server.js now watches src directory for changes and will reload
      components when this happens.

--- src/server/server.js
+++ src/server/server.js
@@ -4,10 +4,12 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import session from 'express-session';
+import chokidar from 'chokidar';
 
 import * as routes from './routes/';
 import conf from './configure';
 import sessionConfig from './helpers/session';
+import { clearRequireCache } from './helpers/hot-load';
 
 const logsPath = conf.get('SERVER:LOGS_PATH') || path.join(__dirname, '../../logs/');
 const accessLogStream = fs.createWriteStream(
@@ -31,5 +33,18 @@ app.use(Express.static(__dirname + '/../public', { maxAge: '365d' }));
 app.use('/', routes.login);
 app.use('/api', routes.api);
 app.use('/', routes.universal);
+
+if (process.env.NODE_ENV === 'development') {
+  // Do "hot-reloading" of express stuff on the server
+  // Throw away cached modules and re-require next time
+  // Ensure there's no important state in there!
+  const watcher = chokidar.watch('./src');
+
+  watcher.on('ready', function() {
+    watcher.on('all', function() {
+      clearRequireCache(/[\/\\]src[\/\\]/, require.cache);
+    });
+  });
+}
 
 export { app as default };
